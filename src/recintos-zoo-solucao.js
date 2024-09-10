bd2c655df6230ec0d2053512f9aa39508ffd56c6
Rachel Pizane <rachel.pzm@gmail.com>
2024-09-10 17:54:09 -0300

    feat: populei o projeto com os recintos existentes e os animais habilitados

--- src/recintos-zoo-solucao.js
+++ src/recintos-zoo-solucao.js
@@ -38,6 +38,30 @@ class Resultado {
   }
 }
 
+// Animais que o zoologico está habilitado a tratar
+const leao = new Animal("leao", true, 3, ["savana"])
+const leopardo = new Animal("leopardo", true, 2, ["savana"])
+const crocodilo = new Animal("crocodilo", true, 3, ["rio"])
+const macaco = new Animal("macaco", false, 1, ["savana", "floresta"])
+const gazela = new Animal("gazela", false, 2, ["savana"])
+const hipopotamo = new Animal("hipopotamo", false, 4, ["savana", "rio"])
+
+const animais = [leao, leopardo, crocodilo, macaco, gazela, hipopotamo]
+
+// Recintos existentes
+const recintos = [
+  new Recinto(1, ["savana"], 10, [
+    new AnimalExistente(macaco.tipo, macaco.ehCarnivoro, macaco.tamanho, 3)
+  ]), 
+  new Recinto(2, ["floresta"], 5), 
+  new Recinto(3, ["savana", "rio"], 7, [
+    new AnimalExistente(gazela.tipo, gazela.ehCarnivoro, gazela.tamanho, 1),
+  ]),
+  new Recinto(4, ["rio"], 8),
+  new Recinto(5, ["savana"], 9, [
+    new AnimalExistente(leao.tipo, leao.ehCarnivoro, leao.tamanho, 1)
+  ])
+]
 
 
 function analisaRecintosSolucao(animal, quantidade) {
