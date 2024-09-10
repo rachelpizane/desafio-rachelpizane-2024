65bc9cf53ee9124e631a101206ce08e16e3ebcc7
Rachel Pizane <rachel.pzm@gmail.com>
2024-09-10 17:34:11 -0300

    feat: criei as classes

--- src/recintos-zoo-solucao.js
+++ src/recintos-zoo-solucao.js
@@ -1,3 +1,45 @@
+class Animal {
+  constructor(tipo, ehCarnivoro, tamanho, biomas){
+    this.tipo = tipo;
+    this.ehCarnivoro = ehCarnivoro;
+    this.tamanho = tamanho;
+    this.biomas = biomas;
+  }
+}
+
+class AnimalExistente extends Animal {
+    constructor(tipo, ehCarnivoro, tamanho, quantidade){
+    super(tipo, ehCarnivoro, tamanho)
+    this.quantidade = quantidade
+  }
+  tamanhoTotal() {
+      return this.tamanho * this.quantidade
+  }
+}
+
+class Recinto {
+  constructor(numero, biomas, tamanhoTotal, animais = []){
+    this.numero = numero;
+    this.biomas = biomas;
+    this.tamanhoTotal = tamanhoTotal;
+    this.animais = animais
+  }
+  espacoAtual() {
+    return this.tamanhoTotal - this.animais.reduce((acc, animal) => {
+      return acc + animal.tamanhoTotal()
+    }, 0)
+  }
+}
+
+class Resultado {
+  constructor(erro = 0, recintosViaveis = 0){
+    this.erro = erro;
+    this.recintosViaveis = recintosViaveis;
+  }
+}
+
+
+
 function analisaRecintosSolucao(animal, quantidade) {
  
 }
