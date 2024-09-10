010abfd6e425ef934c9052fe13107ac16b58ad2d
Rachel Pizane <rachel.pzm@gmail.com>
2024-09-10 17:30:31 -0300

    iniciei o projeto

---
+++ src/recintos-zoo-solucao.js
@@ -0,0 +1,3 @@
+function analisaRecintosSolucao(animal, quantidade) {
+ 
+}
---
+++ src/recintos-zoo.js
@@ -0,0 +1,10 @@
+class RecintosZoo {
+
+    analisaRecintos(animal, quantidade) {
+        return {erro: frase , recintosViaveis: 0}
+    }
+
+}
+
+export { RecintosZoo as RecintosZoo };
+
